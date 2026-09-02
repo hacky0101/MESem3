10d8f9167d1bcde66d70c956ce85b892e11c97cb
hacky0101 <hackermeetdeveloper001@gmail.com>
2026-09-02 19:22:54 +0530

    Update chat.mjs

--- api/chat.mjs
+++ api/chat.mjs
@@ -1,10 +1,6 @@
 export default function handler(req, res) {
-    console.log("MESem3 API HIT");
-    console.log("METHOD:", req.method);
-    console.log("BODY:", req.body);
-
     return res.status(200).json({
         success: true,
-        message: "MESem3 API is working!"
+        message: "MESem3 API WORKING"
     });
 }
